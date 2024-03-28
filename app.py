63074e7ed799a3ff5d30612fd6e19b79cfe4d48a
kirtanpatel01 <159251485+kirtanpatel01@users.noreply.github.com>
2024-03-28 07:59:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site3'
+app.secret_key = '8692'
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), )
+    password = db.Column(db.String(20), )
+    email = db.Column(db.String(50), )
+    phone = db.Column(db.String(20), )
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+with app.app_context():
+    db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        phone_number = request.form['phone']
+
+        if User.query.filter_by(username=username).first():
+            return redirect(url_for('register'))
+
+        user = User(username=username, password=generate_password_hash(password), email=email, phone=phone_number)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not check_password_hash(user.password, password):
+            return redirect(url_for('login'))
+
+        login_user(user)
+        flash("hahaha")
+
+        return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
